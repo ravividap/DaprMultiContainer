3ce482d3555c43c0435b4673748141849f0be1ff
ravividap <ravividap@ymail.com>
2022-08-28 20:16:40 +0530

    Dapr multicontainer

---
+++ DaprMultiContainer/DaprFrontEnd/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Google.Api;
+
+namespace DaprFrontEnd.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly DaprClient _daprClient;
+
+        public IndexModel(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+
+        public async Task OnGet()
+        {
+            var forcasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+                HttpMethod.Get,
+                "DaprBackEnd",
+                "weatherforecast"
+                );
+
+            ViewData["WeatherForecastData"] = forcasts;
+        }
+    }
+}
